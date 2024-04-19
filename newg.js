eb0b502d4b2820e8f8f72f01574bbdd83b820651
priyanshu7101 <103735560+priyanshu7101@users.noreply.github.com>
2024-04-19 10:21:08 +0530

    Update newg.js

--- newg.js
+++ newg.js
@@ -110,7 +110,7 @@ function draw() {
   strokeWeight(1);
   text("Reset", 400+20, 800+45);
     if(touches.length>0)
-    if (touches[0].x>400&&touches[0].x<650&&touches[0].y>800&&touches[0].y<860)
+    if (touches[0].x>400*scaling.x&&touches[0].x<650*scaling.x&&touches[0].y>800*scaling.y&&touches[0].y<860*scaling.y)
     {b1.pos=createVector(250,250);
      b1.score*=0.8;
     }
